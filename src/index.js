01ed51131cb840a88172893ae108d4c2b52a76e2
Alfredo Bautista <alfredo@MacBook-Pro-de-Alfredo.local>
2020-10-22 20:27:16 +0200

    add Flutterista hastag

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const functions = require('./functions');
 
 const searchAndRetweet = () => {
   functions
-    .searchLastTweetIdWithHastag('#flutter')
+    .searchLastTweetIdWithHastag('#flutter,#Flutterista,#Flutteristas')
     .on('start', (_) => console.log('Listening...'))
     .on('data', ({ id_str }) => functions.retweetLatest(id_str));
 };
