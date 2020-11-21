41ffa94d865c426a3f3fedf1113d86404e55fea2
Alfredo Bautista <alfredo@MacBook-Pro-de-Alfredo.local>
2020-11-21 20:11:29 +0100

    add new hastags to an array

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const functions = require('./functions');
+const hastags = ['#flutter,#Flutterista,#Flutteristas, #flutterdev','#fluttercommunity'];
 
 const searchAndRetweet = () => {
   functions
-    .searchLastTweetIdWithHastag('#flutter,#Flutterista,#Flutteristas')
+    .searchLastTweetIdWithHastag(hastags)
     .on('start', (_) => console.log('Listening...'))
     .on('data', ({ id_str }) => functions.retweetLatest(id_str));
 };
